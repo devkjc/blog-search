7f4c87be06ba81152c41badcbfe6ddc52d16f224
JongChan <kjc3590@gmail.com>
2023-03-18 11:55:09 +0900

    init: blog-search

---
+++ src/test/java/com/kakao/blogsearch/BlogSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakao.blogsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
